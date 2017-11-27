0aec3ec6994be37f24a13030fd3b0cfda818f82f
David Petringa <david@ajaxtransport.com>
2017-11-27 07:51:15 -0500

    changed file name to better reflect its use

---
+++ resources/views/admin/dispatcher/officeshow.blade.php
@@ -0,0 +1,26 @@
+{{-- Display a list of dispatch offices --}}
+@extends('layouts.master-admin')
+
+@section('title')
+Show dispatch office
+@endsection
+
+@section('content')
+<h2>Dispatch Offices</h2>
+<ul>
+    @foreach ($dispatchers as $dispatcher)
+    <li><a href='contact/{{ $dispatcher['id'] }}/show/'>{{ $dispatcher['office_name'] }}</a>
+
+        <form method='POST' action='/dispatcher/dispatcher/delete'>
+            {{ method_field('put') }}
+
+            {{ csrf_field() }}
+            <input type='hidden' name='id' id='id' value='{{ $dispatcher['id'] }}'>
+            <input type='hidden' name='office_name' value='{{ $dispatcher['office_name'] }}'>
+            <input class="btn btn-danger" type='submit' value='Delete' class='btn btn-primary btn-small'>
+        </form>
+
+    </li>
+    @endforeach
+</ul>
+@endsection
